889d198e7b3b2963d0f0e67072332bc9d0d2337f
Markus Dobmann <md@noqq.eu>
2015-06-18 13:08:39 +0200

    Make it work

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var request = require('request');
+
+function fetchKeys(username) {
+    return new Promise(function (resolve, reject) {
+        request(`https://github.com/${username}.keys`, function (err, res, body) {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            if (res.statusCode >= 300) {
+                err = new Error('Response with status code: ' + res.statusCode);
+                err.statuscode = res.statusCode;
+                reject(err);
+                return;
+            }
+
+            resolve({
+                userName: username,
+                keys: body.split('\n')
+            });
+        });
+    });
+}
+
+module.exports = fetchKeys;
